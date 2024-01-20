30667c57d64fe8640f50db93280a2560f2686e10
renatinhafront <re.saraiva09@hotmail.com>
2024-01-19 21:06:21 -0300

    adicionei o bloco de dados lista dentro do data.js

--- test/data.js
+++ test/data.js
@@ -1,5 +1,19 @@
 // Aqui puedes agregar los datos que necesites para tus pruebas
 // al menos debe tener uno que se llame "data"
 
-export const data = [];
+export const data = [
+  {
+    "id": 1, //"venus-flytrap",
+    "name": "Dionaea muscipula",
+    "shortDescription": "Conhecida como Armadilha de Vênus, é uma das mais famosas plantas carnívoras.",
+    "description": "A Dionaea muscipula é nativa das áreas úmidas dos Estados Unidos e é famosa por suas folhas em forma de armadilha.",
+    "imageUrl": "https://images.tcdn.com.br/img/img_prod/1061386/planta_carnivora_dionaea_muscipula_g3_x_g14_25_1_15e66452c7b491d3fbc1d5acc68669c6.jpg ",
+    "imageUrl2": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/Venus_Flytrap_showing_trigger_hairs.jpg/266px-Venus_Flytrap_showing_trigger_hairs.jpg ",
+    "facts": {
+      "altura": 0.3,
+      "preco": 5,
+      "facilidadeCriacao": "Fácil"
+    }
+  }
+];
 
