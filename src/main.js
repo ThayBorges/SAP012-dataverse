f2a07da5c8c226bce84e1c55ec8ac6efa1b99d59
renatinhafront <re.saraiva09@hotmail.com>
2024-01-19 21:12:15 -0300

    removi códigos desnecessários seguindo a nova semanica do html

--- src/main.js
+++ src/main.js
@@ -3,17 +3,13 @@ import { renderItems } from './view.js';
 
 import data from './data/dataset.js';
 
-//const root = document.querySelector('[id= "root"]')
-//root.innerHTML = renderItems(data);
+const root = document.getElementById("root")
+root.innerHTML = renderItems(data);
 
-const plantList = document.getElementById('plant__list')
-plantList.innerHTML = renderItems(data);
 
-const selectTipo = document.getElementById("tipo")
-const selectNome = document.getElementById("name")
+const selectNome = document.querySelector("[name='selecionar__filtro']")
 const button = document.getElementById("botao-limpar");
 button.addEventListener('click', function () {
-  selectTipo.value = "default"
   selectNome.value = "default"
 });
 
