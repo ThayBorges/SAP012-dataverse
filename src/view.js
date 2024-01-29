b4f04889af4217763469d89e6d97f64c96e3034b
renatinhafront <re.saraiva09@hotmail.com>
2024-01-29 20:24:15 -0300

    funcao map add

--- src/view.js
+++ src/view.js
@@ -1,23 +1,34 @@
 export const renderItems = (data) => {
-
+  const data2 = data.map((item) => {
+    return {
+      name: item.name,
+      imageUrl: item.imageUrl,
+      shortDescription: item.shortDescription,
+      alturaPlanta: item.facts.alturaPlanta,
+      precoPlanta: item.facts.precoPlanta,
+      tempoDeVida: item.facts.tempoDeVida,
+      facilidadeCriacao: item.facts.facilidadeCriacao,
+      capturaPresas: item.facts.capturaPresas
+    }
+  })
   let blocoItems = ""
-  data.forEach(item => {
+  data2.forEach(item => {
     blocoItems += `
         <li itemscope itemtype="plant__item" class="plant__item">
           <div class="plant__content">
             <img src="${item.imageUrl}" class="plant__img">
             <p><strong>${item.name}</strong></p>
             <p class="plant__description">${item.shortDescription}</p>
-            <p>Altura em cm: ${item.facts.alturaPlanta}</p>
-            <p>Preço: R$ ${item.facts.precoPlanta.toFixed(2)}</p>
-            <p>Tempo de vida em anos: ${item.facts.tempoDeVida}</p>
-            <p>Facilidade de Criação: ${item.facts.facilidadeCriacao}</p>
-            <p>Tipo de captura das presas: ${item.facts.capturaPresas}</p>
+            <p>Altura em cm: ${item.alturaPlanta}</p>
+            <p>Preço: R$ ${item.precoPlanta.toFixed(2)}</p>
+            <p>Tempo de vida em anos: ${item.tempoDeVida}</p>
+            <p>Facilidade de Criação: ${item.facilidadeCriacao}</p>
+            <p>Tipo de captura das presas: ${item.capturaPresas}</p>
           </div>
         </li>
   `
   });
-  return `<ul id="plant__list">${blocoItems}</ul>`;
+  return blocoItems;
 };
 
 export const renderFilter = (filter) => {
@@ -31,13 +42,13 @@ export const renderFilter = (filter) => {
     <option value="5">5 anos</option>
     <option value="10">10 anos</option>
     `
-  }else if (filter === "capturaPresas"){
-    blocoFilter =`
+  } else if (filter === "capturaPresas") {
+    blocoFilter = `
     <option value="Ativa">Ativa</option>
     <option value="Semiativa">Semiativa</option>
     <option value="Passiva">Passiva</option>
     `
-  }else {
+  } else {
     blocoFilter = `
     <option value="Fácil">Fácil</option>
     <option value="Moderado">Moderado</option>
@@ -47,8 +58,3 @@ export const renderFilter = (filter) => {
   return blocoFilter
 };
 
-
-// $ é interpolação
-// variáveis precisam ser inseridas com um sifrão ($) e entre chaves ({nome})
-// string entre crases (`)
-//Para adicionar a segunda imagem <img src="${data[i].imageUrl2}" class="plant__img"> /   <li>${data[i].shortDescription}</li>
