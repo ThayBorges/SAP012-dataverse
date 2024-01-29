458abfa5daeaf6c6278a5d64b98d5fdec4d2d58b
renatinhafront <re.saraiva09@hotmail.com>
2024-01-29 20:23:54 -0300

    calculo da média incluido

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import { renderItems, renderFilter } from './view.js';
 import data from './data/dataset.js';
-import { sortBy, filterBy } from './dataFunctions.js';
+import { sortBy, filterBy, calculaMediaVida, calculaMediaAltura } from './dataFunctions.js';
 
 
-const root = document.getElementById("root")
-root.innerHTML = renderItems(data);
+const root = document.getElementById("root");
+const plants = document.createElement("ul");
+plants.setAttribute("class", "plant__list");
+root.appendChild(plants)
+plants.innerHTML = renderItems(data);
 
 const selectSubFilter = document.getElementById("select__subFilter");
 const selectFilter = document.getElementById("select__filter");
@@ -13,13 +16,25 @@ selectFilter.addEventListener('change', function () {
   selectSubFilter.innerHTML = renderFilter(selectFilter.value)
 });
 
-const button = document.getElementById("botao-buscar");
+//Calcula a media de vida no span sobre elas
+const mediaVida = document.querySelector(".mediaVida");
+mediaVida.innerHTML = calculaMediaVida(data);
+
+//calcula a media altura no span sobre elas
+const mediaAltura = document.querySelector(".mediaAltura");
+mediaAltura.innerHTML = calculaMediaAltura(data);
+
+const button = document.querySelector("#botao-buscar");
 button.addEventListener('click', function () {
-  const filtroSelecionado = document.getElementById("select__subFilter").value;
-  const ordenacaoSelecionada = document.getElementById("select__ordering").value;
+  const filtroSelecionado = document.querySelector("#select__subFilter").value;
+  const ordenacaoSelecionada = document.querySelector("#select__ordering").value;
   const ordernacaoAscDesc = document.querySelector('input[name="ordering"]:checked').value;
 
   const meuDataSetFiltrado = filterBy(data, filtroSelecionado);
   const meuDataSetOrdenado = sortBy(meuDataSetFiltrado, ordenacaoSelecionada, ordernacaoAscDesc);
   root.innerHTML = renderItems(meuDataSetOrdenado);
+
+  //recalcula as medias de acordo com os filtros
+  mediaAltura.innerHTML = calculaMediaAltura(meuDataSetOrdenado);
+  mediaVida.innerHTML = calculaMediaVida(meuDataSetOrdenado);
 });
