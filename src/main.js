0ba37dd8ab40fa8677944232c57460b0ba3eec90
renatinhafront <re.saraiva09@hotmail.com>
2024-01-31 09:31:28 -0300

    crei novos eventos de pra filtar e ordenar

--- src/main.js
+++ src/main.js
@@ -15,8 +15,24 @@ body.insertBefore(imgLogoFooter, footer)
 const selectSubFilter = document.getElementById("select-subFilter");
 const selectFilter = document.getElementById("select-filter");
 selectSubFilter.innerHTML = renderFilter(selectFilter.value)
+
 selectFilter.addEventListener('change', function (event) {
   selectSubFilter.innerHTML = renderFilter(event.target.value)
+  ordenarEfiltrar()
+});
+
+selectSubFilter.addEventListener('change', function () {
+  ordenarEfiltrar()
+});
+
+const selectSort = document.getElementById("select-sort");
+const selectOrder = document.getElementById("select-order");
+selectSort.addEventListener('change', function () {
+  ordenarEfiltrar()
+});
+
+selectOrder.addEventListener('change', function () {
+  ordenarEfiltrar()
 });
 
 //Calcula a media de vida no span sobre elas
@@ -31,17 +47,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
 const button = document.querySelector("#button-clear");
 button.addEventListener('click', function () {
-  const filtroSelecionado = document.querySelector("#select-subFilter").value;
-  const ordenacaoSelecionada = document.querySelector("#select-sort").value;
-  const ordernacaoAscDesc = document.querySelector('input[name="order"]:checked').value;
+  selectSort.value = "name"
+  selectOrder.value = "asc"
+  selectSubFilter.value = "facil"
+  selectFilter.value = "facilidadeCriacao"
 
-  const meuDataSetFiltrado = filterBy(data, filtroSelecionado);
-  const meuDataSetOrdenado = sortBy(meuDataSetFiltrado, ordenacaoSelecionada, ordernacaoAscDesc);
-  root.innerHTML = renderItems(meuDataSetOrdenado);
-
-  //recalcula as medias de acordo com os filtros
-  mediaAltura.innerHTML = calculaMediaAltura(meuDataSetOrdenado);
-  mediaVida.innerHTML = calculaMediaVida(meuDataSetOrdenado);
-
+  ordenarEfiltrar()
 });
 
+function ordenarEfiltrar() {
+  const meuDataSetFiltrado = filterBy(data, selectSubFilter.value);
+  const meuDataSetOrdenado = sortBy(meuDataSetFiltrado, selectSort.value, selectOrder.value);
+
+  root.innerHTML = renderItems(meuDataSetOrdenado);
+
+  mediaAltura.innerHTML = calculaMediaAltura(meuDataSetOrdenado);
+  mediaVida.innerHTML = calculaMediaVida(meuDataSetOrdenado);
+}
