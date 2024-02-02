d50da9ad11c47277309777af70f2b0b9bc99557f
renatinhafront <re.saraiva09@hotmail.com>
2024-02-02 09:31:38 -0300

    organizando a main

--- src/main.js
+++ src/main.js
@@ -2,19 +2,22 @@ import { renderItems, renderFilter } from './view.js';
 import data from './data/dataset.js';
 import { sortBy, filterBy, calculaMediaVida, calculaMediaAltura } from './dataFunctions.js';
 
+
+//selectors html
 const root = document.querySelector("#root");
-root.innerHTML = renderItems(data);
-
+const selectSort = document.querySelector("#select-sort");
+const selectOrder = document.querySelector("#select-order");
 const footer = document.querySelector("footer");
-const body = footer.parentNode;
-const imgLogoFooter = document.createElement("img");
-imgLogoFooter.setAttribute("id", "logo__footer");
-imgLogoFooter.src = "/assets/logo.jpg";
-body.insertBefore(imgLogoFooter, footer)
-
 const selectSubFilter = document.getElementById("select-subFilter");
 const selectFilter = document.getElementById("select-filter");
-selectSubFilter.innerHTML = renderFilter(selectFilter.value)
+const button = document.querySelector("#button-clear");
+const mediaVida = document.querySelector(".mediaVida");
+const mediaAltura = document.querySelector(".mediaAltura");
+
+//listerns
+selectOrder.addEventListener('change', function () {
+  ordenarEfiltrar()
+});
 
 selectFilter.addEventListener('change', function (event) {
   selectSubFilter.innerHTML = renderFilter(event.target.value)
@@ -25,27 +28,10 @@ selectSubFilter.addEventListener('change', function () {
   ordenarEfiltrar()
 });
 
-const selectSort = document.querySelector("#select-sort");
-const selectOrder = document.querySelector("#select-order");
 selectSort.addEventListener('change', function () {
   ordenarEfiltrar()
 });
 
-selectOrder.addEventListener('change', function () {
-  ordenarEfiltrar()
-});
-
-//Calcula a media de vida no span sobre elas
-const mediaVida = document.querySelector(".mediaVida");
-//calcula a media altura no span sobre elas
-const mediaAltura = document.querySelector(".mediaAltura");
-document.addEventListener("DOMContentLoaded", function () {
-
-  mediaVida.innerHTML = calculaMediaVida(data);
-  mediaAltura.innerHTML = calculaMediaAltura(data);
-});
-
-const button = document.querySelector("#button-clear");
 button.addEventListener('click', function () {
   root.innerHTML = renderItems(data);
   selectFilter.value = '';
@@ -54,12 +40,29 @@ button.addEventListener('click', function () {
   selectOrder.value = '';
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  mediaVida.innerHTML = calculaMediaVida(data);
+  mediaAltura.innerHTML = calculaMediaAltura(data);
+});
+
+//functions
 function ordenarEfiltrar() {
   const meuDataSetFiltrado = filterBy(data, selectSubFilter.value);
   const meuDataSetOrdenado = sortBy(meuDataSetFiltrado, selectSort.value, selectOrder.value);
 
   root.innerHTML = renderItems(meuDataSetOrdenado);
-
   mediaAltura.innerHTML = calculaMediaAltura(meuDataSetOrdenado);
   mediaVida.innerHTML = calculaMediaVida(meuDataSetOrdenado);
 }
+
+//initialize
+root.innerHTML = renderItems(data);
+
+const body = footer.parentNode;
+const imgLogoFooter = document.createElement("img");
+imgLogoFooter.setAttribute("id", "logo__footer");
+imgLogoFooter.src = "/assets/logo.jpg";
+body.insertBefore(imgLogoFooter, footer)
+
+
+selectSubFilter.innerHTML = renderFilter(selectFilter.value)
